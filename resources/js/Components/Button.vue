16c50b42c6e6c3bb1eb5bc4cab5b162632810396
Abdessamad Bettal | Freelancer <93977120+abdessamadbettal@users.noreply.github.com>
2024-12-02 13:14:20 +0000

    Add new Container and Icon components; refactor Button component to support link functionality

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,34 +1,37 @@
 <script setup>
 import { ref, computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
 
 const props = defineProps({
-  isLoading: {
-    type: Boolean,
-    default: false
-  },
-  variant: {
-    type: String,
-    default: 'primary'
-  }
+    isLoading: {
+        type: Boolean,
+        default: false
+    },
+    variant: {
+        type: String,
+        default: 'primary'
+    },
+    href: {
+        type: String,
+        default: null
+    }
 });
 
 const buttonClasses = computed(() => {
-  switch (props.variant) {
-    case 'danger':
-      return 'inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-red-700 dark:focus:ring-offset-gray-800';
-    case 'outline':
-      return 'inline-flex items-center rounded-md border border-gray-800 bg-transparent px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-800 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-200 dark:border-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus:ring-offset-gray-800 dark:active:bg-gray-600';
-    default:
-      return 'inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300';
-  }
+    switch (props.variant) {
+        case 'danger':
+            return 'inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-red-700 dark:focus:ring-offset-gray-800';
+        case 'outline':
+            return 'inline-flex items-center rounded-md border border-gray-800 bg-transparent px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-800 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-200 dark:border-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus:ring-offset-gray-800 dark:active:bg-gray-600';
+        default:
+            return 'inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300';
+    }
 });
 </script>
 <template>
-    <button
-        :disabled="isLoading"
-        :class="[buttonClasses, { 'opacity-25': isLoading }]"
-    >
+    <component :is="href ? Link : 'button'" :href="href" :disabled="isLoading"
+        :class="[buttonClasses, { 'opacity-25': isLoading }]">
         <slot v-if="!isLoading" />
         <span v-else>Loading...</span>
-    </button>
+    </component>
 </template>
---
+++ resources/js/Components/Layout/Header.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { ref } from "vue"
+import Container from "@/Components/Container.vue"
+import Logo from "@/Components/Icons/Logo.vue"
+import IconBars from "@/Components/Icons/IconBars.vue"
+import IconX from "@/Components/Icons/IconX.vue"
+import Button from "@/Components/Button.vue"
+import { Transition } from "vue"
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    menuItems: {
+        type: Array,
+        required: true,
+        default: () => []
+    },
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+})
+
+
+
+const showMobileMenu = ref(false)
+
+function redirect(link) {
+    showMobileMenu.value = false
+    window.location = link
+}
+</script>
+<template>
+    <header class=" py-6 md:py-10 sticky top-0 z-10 ">
+        <Container class="relative flex items-center justify-between gap-4 text-slate-600 text-sm">
+
+            <div class="flex items-center gap-8">
+                <a href="#">
+                    <Logo class="h-10" />
+                </a>
+                <nav class="items-center gap-3 hidden md:flex">
+                    <a v-for="menu in menuItems" :key="menu.label" v-text="menu.label" :href="menu.link"
+                        class="rounded-lg py-1.5 px-3 transition-colors duration-300 hover:bg-red-200 font-bold text-red-900" />
+                </nav>
+            </div>
+
+            <div class="flex items-center gap-4">
+                <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                Dashboard
+                </Link>
+
+                <template v-else>
+                    <Button variant="outline" :href="route('login')" class="hidden md:inline" v-if="canLogin">
+                        Login
+                    </Button>
+
+                    <Button :href="route('register')" v-if="canRegister">
+                        Register <span v-text="'now'" class="hidden lg:inline pl-1" />
+                    </Button>
+                </template>
+
+                <!-- MOBILE MENU -->
+                <div class="md:hidden">
+                    <button class="p-2 md:hidden relative z-10" @click.prevent="showMobileMenu = !showMobileMenu">
+                        <component :is="showMobileMenu ? IconX : IconBars" class="w-5"
+                            :class="{ 'text-red-500': showMobileMenu }" />
+                    </button>
+
+                    <Transition enter-from-class="opacity-0" leave-to-class="opacity-0"
+                        class="transition-opacity duration-500">
+                        <div v-show="showMobileMenu" @click.prevent="showMobileMenu = false"
+                            class="fixed inset-0 bg-slate-200/50 backdrop-blur-sm" />
+                    </Transition>
+
+                    <Transition enter-from-class="-translate-x-full opacity-0"
+                        leave-to-class="-translate-x-full opacity-0" class="transition-all duration-500">
+                        <div @click.stop v-show="showMobileMenu"
+                            class="p-4 absolute inset-x-4 top-14 bg-white rounded-xl shadow-lg">
+                            <nav class="flex flex-col">
+                                <button v-for="menu in menuItems" :key="menu.label" v-text="menu.label"
+                                    @click.prevent="redirect(menu.link)"
+                                    class="p-4 hover:bg-slate-100 rounded-lg text-left" />
+                                <hr class="my-2" />
+                                <Link class="p-4 hover:bg-slate-100 rounded-lg text-left" :href="route('login')"
+                                    v-if="canLogin && !$page.props.auth.user">
+                                Login
+                                </Link>
+                            </nav>
+                        </div>
+                    </Transition>
+                </div>
+            </div>
+        </Container>
+    </header>
+</template>
