a92351b687aac87693e6e55556c1d908cc57ddb9
Abdessamad Bettal | Freelancer <93977120+abdessamadbettal@users.noreply.github.com>
2024-12-06 12:05:48 +0000

    Update meta tags in Welcome.vue and change app title to 'Laravel Starter'

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -7,7 +7,14 @@ import { ref } from 'vue';
 
 <template>
 
-    <Head title="Welcome" />
+    <Head>
+        <title>Laravel Inertia Vue 3 Starter</title>
+        <meta name="description"
+            content="Simplify your Laravel development with Laravel Starter! Build faster and smarter with tools like Inertia.js, Vue, Tailwind, multi-language support, SEO optimization, and Filament CMS - all ready to use.">
+        <meta name="keywords"
+            content="Laravel, Inertia.js, Vue, TailwindCSS, multi-language, SEO, Filament CMS, Laravel Starter, SSR, Spatie, i18n">
+    </Head>
+
     <Hero />
 
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,14 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Laravel Starter') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <script>
-            window.locale = '{{ app()->getLocale() }}';
-        </script>
 
         <!-- Scripts -->
         @routes
